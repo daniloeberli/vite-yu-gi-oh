91768b45abfb9bbdba5e2fd71385cb4aab6d002e
daniloeberli <danilo.eberli@gmail.com>
2023-03-27 17:12:09 +0200

    feat: card img

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <script >
 import axios from 'axios';
-import {store} from './store'
+import { store } from './store'
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
-export default{
+export default {
     name: 'App',
-    components:{
+    components: {
         AppHeader,
         AppMain
     },
-    data(){
-        return{
+    data() {
+        return {
             store
         }
     },
-    created(){
-            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-                .then((response) => {
-                    console.log(response);
-                    this.store.characters = response.data.data;
-                    this.store.charactersFound = response.data.data.length;
-                })
-        }
+    created() {
+
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+            .then((response) => {
+                this.store.characters = response.data.data;
+                this.store.charactersFound = response.data.data.length;
+            })
+    }
 }
 
 
 </script>
 
 <template>
-  <AppHeader></AppHeader>
-  <AppMain></AppMain>
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
 </template>
