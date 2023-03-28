5848cc78f8924e77dad18f78188f897cfd3900f1
daniloeberli <danilo.eberli@gmail.com>
2023-03-28 16:00:32 +0200

    feat: header logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,27 @@ export default{
 
 
 <template>
-    <header>
-        <h1>yu-gi-oh API</h1>
+    <header class="d-flex">
+        <div>
+            <img  src="../../src/logo.png" alt="logo">
+        </div>
+        <h1>Yu-Gi-Oh API</h1>
     </header>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+header{
+    padding: 20px 10px;
+    align-items: center;
+}
+
+img{
+    max-width: 100px;
+}
+
+h1{
+    padding-left: 20px;
+}
+</style>
